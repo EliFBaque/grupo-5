2154a2d470cd5488d5dae10b005e3e6c3212b448
ncacioni <nahuelcacioni@gmail.com>
2021-12-03 13:59:21 -0300

    confirmar

---
+++ Clases/Nahuel Gustavo Cacioni/ex01.py
@@ -0,0 +1,16 @@
+class complex:
+    def __init__(self,n1,n2):
+        self.n1 = n1
+        self.n2 = n2
+
+    def sum(self):
+        return self.n1 + self.n2
+
+    def substraction(self,n1,n2):
+        return self.n1 - self-n2
+
+    def multiplication(self,n1,n2):
+        return self.n1 * self.n2
+
+    def division(self,n1,n2):
+        return self.n1 / self.n2
